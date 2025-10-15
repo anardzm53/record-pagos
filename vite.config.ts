641de5799afdc3dfa8b73e92deb32b76b320e11d
= <=>
2025-10-14 19:51:39 -0600

    add script to index.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import csp from "vite-plugin-csp-guard";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(),tailwindcss(),
+    // csp({
+    //   algorithm: "sha256", // The algorithm to use for hashing
+    //   dev: {
+    //     run: true, // If you want to run the plugin in `vite dev` mode
+    //   },
+    //   policy: {
+    //     // Specify the policy here.
+    //     "script-src": ["'self'", "https://www.google-analytics.com"], // Example: Allow Google Analytics
+    //     "style-src": ["'self'", "https://fonts.googleapis.com"], // Example: Allow Google Fonts
+    //   },
+    // })
+  ],
 })
