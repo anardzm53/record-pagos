d49edf0114f16f1aedb09c024f6bff67ee821a4b
= <=>
2025-10-14 19:52:42 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import csp from "vite-plugin-csp-guard";
+// import csp from "vite-plugin-csp-guard";
 
 // https://vite.dev/config/
 export default defineConfig({
