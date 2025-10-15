7983e27d37e54af8ec0284f8e6146d961d2a6944
= <=>
2025-10-14 20:32:47 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import federation from "@originjs/vite-plugin-federation"
 // import csp from "vite-plugin-csp-guard";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
   plugins: [react(),tailwindcss(),
+    federation({
+      name: "remote-app",
+      filename: "remoteEntry.js",
+      //Modules to use
+      exposes: {
+        "./App": "./src/App.tsx",
+      },
+      shared: ["react"],
+    })
     // csp({
     //   algorithm: "sha256", // The algorithm to use for hashing
     //   dev: {
